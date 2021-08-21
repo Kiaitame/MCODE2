80600b9a2fca811538ab2ac35b72b97919d7a6f4
teezziestezos <teezziestezos@penguin>
2021-08-21 19:18:46 +0900

    javascript3-1

--- Todo/todo.js
+++ Todo/todo.js
@@ -8,32 +8,26 @@
                 comment: inputTask,
                 status: '作業中'
             };
+
         tasks.push(task);
         
-        console.log(tasks);
         const tbl = document.getElementById('tbl');
         const tr = document.createElement('tr');
-        // const status = document.createElement('button');
-        // const del = document.createElement('button');
-        // status.id = 'status';
-        // status.innerText = '作業中';
-        // del.id = 'delete';
-        // del.innerText = '削除';
 
         function createTd(contents){
-            var td = document.createElement('td');
+            const td = document.createElement('td');
             td.innerHTML = contents;
             tbl.appendChild(td);    
         }
+        
         tbl.appendChild(tr);
         createTd(tasks.length-1);
         createTd(inputTask);
-        createTd('<button>' + '作業中' + '</button>');
+        createTd('<button>' + task.status + '</button>');
         createTd('<button>' + '削除' + '</button>');
-        // tbl.appendChild(status);
-        // tbl.appendChild(del);
+
+        document.getElementById('input').value = '';
 
     });
-
 
 }
