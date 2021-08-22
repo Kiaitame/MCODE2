a5f0bc07af427c56bd7db0f678376d5ad5375727
teezziestezos <teezziestezos@penguin>
2021-08-22 22:19:04 +0900

    javascript3-1_review

--- Todo/todo.js
+++ Todo/todo.js
@@ -10,24 +10,32 @@
             };
 
         tasks.push(task);
+
+        console.log(tasks);
         
         const tbl = document.getElementById('tbl');
         const tr = document.createElement('tr');
 
-        function createTd(contents){
+
+        const createTd = contents => {
             const td = document.createElement('td');
             td.innerHTML = contents;
-            tbl.appendChild(td);    
+            tr.appendChild(td);
         }
+                
         
+        console.log(tasks);
+
+        tasks.forEach((element) => {
+            createTd(tasks.indexOf(element));
+            createTd(element.comment);
+            createTd('<button>' + element.status + '</button>');
+            createTd('<button id="del">' + '削除' + '</button>');
+        })
         tbl.appendChild(tr);
-        createTd(tasks.length-1);
-        createTd(inputTask);
-        createTd('<button>' + task.status + '</button>');
-        createTd('<button>' + '削除' + '</button>');
 
         document.getElementById('input').value = '';
-
+        
     });
 
 }
