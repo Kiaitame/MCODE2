fb69b9b4eb18331e499d095ffc93a7ade4be5d42
teezziestezos <teezziestezos@penguin>
2021-08-23 19:03:35 +0900

    javascript3-1_review

--- Todo/todo.js
+++ Todo/todo.js
@@ -11,28 +11,26 @@
 
         tasks.push(task);
 
-        console.log(tasks);
-        
         const tbl = document.getElementById('tbl');
-        const tr = document.createElement('tr');
-
-
-        const createTd = contents => {
-            const td = document.createElement('td');
-            td.innerHTML = contents;
-            tr.appendChild(td);
-        }
-                
         
-        console.log(tasks);
-
-        tasks.forEach((element) => {
-            createTd(tasks.indexOf(element));
-            createTd(element.comment);
-            createTd('<button>' + element.status + '</button>');
-            createTd('<button id="del">' + '削除' + '</button>');
-        })
-        tbl.appendChild(tr);
+        tbl.innerHTML = '';
+        
+        tasks.forEach((value,index) => {
+            const tr  = document.createElement('tr');
+            const td1 = document.createElement('td');
+            td1.innerHTML = index;
+            const td2 = document.createElement('td');
+            td2.innerHTML = value.comment;
+            const td3 = document.createElement('td');
+            td3.innerHTML = '<button>' + value.status + '</button>';
+            const td4 = document.createElement('td');
+            td4.innerHTML = '<button id="del">' + '削除' + '</button>';
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tbl.appendChild(tr);
+        });
 
         document.getElementById('input').value = '';
         
