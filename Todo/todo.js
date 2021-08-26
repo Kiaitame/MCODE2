4ccce9bdb37e9a49bf752f71fdb8956099c658e1
teezziestezos <teezziestezos@penguin>
2021-08-26 14:01:47 +0900

    javascript3-2

--- Todo/todo.js
+++ Todo/todo.js
@@ -1,62 +1,56 @@
 'use strict';
-    const tasks = [];
-        const tbl = document.getElementById('tbl');
+const tasks = [];
+const tbl = document.getElementById('tbl');
+const createTd = (contents) => {
+    const td = document.createElement('td');
+    td.innerHTML = contents;
+    return td;
+}
 
-        const createTd = (contents) => {
-            const td = document.createElement('td');
-            td.innerHTML = contents;
-            return td;
-        }
+
+//ステータスボタン生成処理
+const createStatusBtn = (index) => {
+    const sts = document.createElement('button');
+    sts.setAttribute('id','crebtn_' + index);
+    sts.innerHTML = '作業中';
+    return sts;
+}
 
 
-        //ステータスボタン生成処理
-        const createStatusBtn = (idx) => {
-            const sts = document.createElement('button');
-            sts.setAttribute('id','crebtn_' + idx);
-            sts.innerHTML = '作業中';
-            return sts;
-          }
-          //削除ボタン生成処理
-        const createRemoveBtn = (idx) => {
-            const rmv = document.createElement('button');
-            rmv.setAttribute('id', 'rmvbtn_' + idx);
-            const idx2 = idx.rowIndex -1;
-            rmv.innerHTML = '削除';
-            rmv.addEventListener('click',() => {
-                tasks.splice(idx,1);
-                TodoDispay();
-            });
-            return rmv;
-        }
+//削除ボタン生成処理
+const createRemoveBtn = (index) => {
+    const rmv = document.createElement('button');
+    rmv.setAttribute('id', 'rmvbtn_' + index);
+    rmv.innerHTML = '削除';
+    rmv.addEventListener('click',() => {
+        tasks.splice(index,1);
+        TodoDispay();
+    });
+    return rmv;
+}
 
-        const TodoDispay = () => {
-            tbl.innerHTML = '';
-            tasks.forEach((value,index) => {
-                const tr  = document.createElement('tr');
-                tr.appendChild(createTd(index));
-                tr.appendChild(createTd(value.comment));
-                tr.appendChild(createStatusBtn(index));
-                tr.appendChild(createRemoveBtn(index));
-                tbl.appendChild(tr);
-            });
 
-            document.getElementById('input').value = '';
-        }
+const TodoDispay = () => {
+    tbl.innerHTML = '';
+    tasks.forEach((value,index) => {
+        const tr  = document.createElement('tr');
+        tr.appendChild(createTd(index));
+        tr.appendChild(createTd(value.comment));
+        tr.appendChild(createStatusBtn(index));
+        tr.appendChild(createRemoveBtn(index));
+        tbl.appendChild(tr);
+    });
+    document.getElementById('input').value = '';
+}
 
 
 document.getElementById('add').addEventListener('click',() => {
-    const inputTask = document.getElementById('input').value;
-    
-    const task = {
-             comment: inputTask,
-             status: '作業中'
-                };
-    
-    tasks.push(task);
-
-    TodoDispay();
+const inputTask = document.getElementById('input').value;
+const task = {
+        comment: inputTask,
+        status: '作業中'
+        };
+tasks.push(task);
+TodoDispay();
 });
-    
-
-   
 
